14ec66d81dbbb03383e30e90e36d8a7561c1bc31
Bart Solo <yosoybartsolo@gmail.com>
2022-11-07 09:02:40 -0600

    MongoDB connected and project started

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home({ isConnected }) {
       <div className="content flex justify-center items-center w-full my-16">
         <div className="wrapper max-w-7xl">
           <h1 className="text-2xl text-center font-bold">
-            Molusco 🦑 <br /> Nextjs + Tailwind Starter!{" "}
+            Molusco - aucoin 🦑 <br /> Nextjs + Tailwind Starter!{" "}
           </h1>
           <br />
           <div className="connectioncontainer">
