8dcd234d77d7da7cea4d6bb5f1a10911c986737f
Bart Solo <yosoybartsolo@gmail.com>
2022-11-13 00:06:15 -0600

    bid history json n' stuff

--- components/common/BidHistory.js
+++ components/common/BidHistory.js
@@ -1,107 +1,103 @@
-//component that displays the bid history of a particular item
 import React, { Component } from "react";
+import LoadingCircle from "@/components/common/LoadingCircle";
+
+const isInitialLoading = false;
+const fetchError = false;
+const bidz = undefined;
+const page = 1;
+const paginationData = {};
+
+const bids = [
+  {
+    bidder: "0x123456789",
+    amount: 0.1,
+    timestamp: "2021-05-01T00:00:00.000Z",
+  },
+  {
+    bidder: "0x123456789",
+    amount: 0.4,
+    timestamp: "2021-05-01T00:00:00.000Z",
+  },
+  {
+    bidder: "0x123456789",
+    amount: 1.0,
+    timestamp: "2021-05-01T00:00:00.000Z",
+  },
+];
+
+console.log(bids);
 
 export default class BidHistory extends Component {
   render() {
     return (
-      <div className="flex flex-col">
+      <div className="flex flex-col px-4">
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <div>
-                <h2 className="text-2xl text-center font-bold my-4">
-                  Historial de pujas
-                </h2>
+            {isInitialLoading ? (
+              <div className="py-24">
+                <LoadingCircle color="#000000" />
               </div>
-              <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Precio
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Fecha
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Origen
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
-                  <tr>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      <div className="flex items-center">
-                        <div className="ml-4">
-                          <div className="text-sm font-medium text-gray-900">
-                            1 SOL
+            ) : fetchError ? (
+              <div className="py-24 text-center">
+                <p className="text-lg text-gray-500">
+                  Hubo un error al cargar las subastas
+                </p>
+              </div>
+            ) : bids && bids.length > 0 ? (
+              <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Comprador
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Monto
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Fecha
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200">
+                    {bids.map((bid) => (
+                      <tr key={bid.bidder}>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm text-gray-900">
+                            {bid.bidder}
                           </div>
-                        </div>
-                      </div>
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      <div className="text-sm text-gray-900">
-                        13/11/2022 12:00:00
-                      </div>
-                      <div className="text-sm text-gray-500"></div>
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      0x1234567890
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      <div className="flex items-center">
-                        <div className="ml-4">
-                          <div className="text-sm font-medium text-gray-900">
-                            0.9 SOL
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm text-gray-900">
+                            {bid.amount}
                           </div>
-                        </div>
-                      </div>
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      <div className="text-sm text-gray-900">
-                        13/11/2022 12:00:00
-                      </div>
-                      <div className="text-sm text-gray-500"></div>
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      0x1234567890
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      <div className="flex items-center">
-                        <div className="ml-4">
-                          <div className="text-sm font-medium text-gray-900">
-                            0.8 SOL
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm text-gray-900">
+                            {bid.timestamp}
                           </div>
-                        </div>
-                      </div>
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      <div className="text-sm text-gray-900">
-                        13/11/2022 12:00:00
-                      </div>
-                      <div className="text-sm text-gray-500"></div>
-                    </td>
-
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      0x1234567890
-                    </td>
-                  </tr>
-                  {/* More items... */}
-                </tbody>
-              </table>
-            </div>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            ) : (
+              <div className="py-24 text-center">
+                <p className="text-lg text-gray-500">
+                  No se encontraron subastas
+                </p>
+              </div>
+            )}
           </div>
         </div>
       </div>
